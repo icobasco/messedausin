d52df440b2244777a51138d58618aa6fa24d803e
icobasco <enrico.allione@gmail.com>
2023-02-11 18:28:52 +0100

    Fix repository

--- js/chiese_script.js
+++ js/chiese_script.js
@@ -4,22 +4,43 @@
  */
 
 const caricaChiese =()=> {
+    console.log("caricaChiese()");
+    let mapIconHTML = "<img src=\"./img/map_50.png\"/>";
     let divChiese = document.querySelector("#divChiese");
     while (divChiese.hasChildNodes()) {
         divChiese.removeChild(divChiese.children[0]);
     }
+    let oldPaese = "";
     for (let idChiesa=0; idChiesa<chiese.length; idChiesa++){
         let chiesa = chiese[idChiesa];
         console.log("Chiesa: " + chiesa.intitolazione);
         let divChiesa = document.createElement("div");
         divChiesa.className = "chiesa"; 
-        let mappa = "";
+        let divMappa = "";
+        if (chiesa.paese != oldPaese) {
+            let divPaese = document.createElement("div");
+            divPaese.className = "paese";
+            divPaese.innerHTML = "<b>" + chiesa.paese + "</b>";
+            divChiese.appendChild(divPaese);
+            oldPaese = chiesa.paese;
+        }
         if (chiesa.posizione.lat != 0 && chiesa.posizione.lon != 0){
-            mappa = " <a target=\"_blank\" href=\"https://www.google.com/maps/@" + chiesa.posizione.lat + "," + chiesa.posizione.lon + ",19z\"> MAPPA </a>";
+            divMappa = document.createElement("div");
+            divMappa.innerHTML = " <a class=\"map_icon\" target=\"_blank\" href=\"https://www.google.com/maps/@" + chiesa.posizione.lat + "," + chiesa.posizione.lon + ",19z\">" + mapIconHTML + "</a>";
             // console.log("\t\t Trovato posizione!");
         }
-        divChiesa.innerHTML = 
-            "<b>" + chiesa.intitolazione + "</b></a> di " + chiesa.paese + " (" + chiesa.posizione.indirizzo + ")" + mappa;
+        // TODO: Indirizzo da inserire
+        // let chiesaIndirizzo = "";
+        // if (chiesa.posizione.indirizzo != ""){
+        //     chiesaIndirizzo = " (" + chiesa.posizione.indirizzo + ")";
+        // }
+        let divChiesaName = document.createElement("div");
+        divChiesaName.className = "chiesa_name align-middle h3";
+        // divChiesaName.innerHTML = chiesa.intitolazione + " di " + chiesa.paese + chiesaIndirizzo;
+        divChiesaName.innerHTML = chiesa.intitolazione + " di " + chiesa.paese;
+        divChiesa.appendChild(divChiesaName);
+        if (divMappa != "")
+            divChiesa.appendChild(divMappa);
         divChiese.appendChild(divChiesa);
     }
 }
