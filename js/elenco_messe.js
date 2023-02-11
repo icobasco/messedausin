63c8b7e2bd400965b0e7d96e825550cc95997066
icobasco <47712708+icobasco@users.noreply.github.com>
2023-02-11 16:15:46 +0100

    Update elenco_messe.js

--- js/elenco_messe.js
+++ js/elenco_messe.js
@@ -15,7 +15,7 @@
         { "chiesa": "Mad. del Carmine", "paese": "Piasco", "data": "12/02/2023", "prefes": "FES", "ora": "08:30", "note": "" },
         { "chiesa": "S. Giovanni", "paese": "Piasco", "data": "12/02/2023", "prefes": "FES", "ora": "10:00", "note": "" },
         { "chiesa": "Maria V. Assunta", "paese": "Rossana", "data": "12/02/2023", "prefes": "FES", "ora": "11:00", "note": "La 2^ e la 4^ Domenica del mese" },
-        { "chiesa": "S. Antonio", "paese": "Piasco", "data": "12/02/2023", "prefes": "FES", "ora": "18:00", "note": "" },
+        { "chiesa": "Maria V. Assunta", "paese": "Rossana", "data": "12/02/2023", "prefes": "FES", "ora": "18:00", "note": "" },
         
         { "chiesa": "Mad. delle Grazie", "paese": "Rossana", "data": "18/02/2023", "prefes": "PRE", "ora": "15:00", "note": "" },
         { "chiesa": "S. Giovanni", "paese": "Piasco", "data": "18/02/2023", "prefes": "PRE", "ora": "17:00", "note": "Il 1° e 3° Sabato del mese" },
@@ -32,6 +32,6 @@
         { "chiesa": "Mad. del Carmine", "paese": "Piasco", "data": "26/02/2023", "prefes": "FES", "ora": "08:30", "note": "" },
         { "chiesa": "S. Giovanni", "paese": "Piasco", "data": "26/02/2023", "prefes": "FES", "ora": "10:00", "note": "" },
         { "chiesa": "Maria V. Assunta", "paese": "Rossana", "data": "26/02/2023", "prefes": "FES", "ora": "11:00", "note": "La 2^ e la 4^ Domenica del mese" },
-        { "chiesa": "S. Antonio", "paese": "Piasco", "data": "26/02/2023", "prefes": "FES", "ora": "18:00", "note": "" }	
+        { "chiesa": "Maria V. Assunta", "paese": "Rossana", "data": "26/02/2023", "prefes": "FES", "ora": "18:00", "note": "" },
         
     ]
